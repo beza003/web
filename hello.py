956cf288d0d357a01aad922309d5bdfb881662e5
beza003 <beza003@outlook.com>
2016-03-09 15:58:15 +0000

    1.9 4

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-def app (environ, start_response)
+bind = "0.0.0.0:8080"
+
+def app (environ, start_response):
 	status = '200 OK'
 	response_headers = [('Content-type' , 'text/plain')]
 	start_response (status, response_headers)
-	resp = environ['QUERY_STRING'].split("&")
-	resp = [item + "\r\n" for item in resp]
-	return resp
+	resp = '\r\n'.join(environ['QUERY_STRING'].split("&"))
+	return [resp]
